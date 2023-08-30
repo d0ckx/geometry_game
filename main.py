215eedd4c04a0c6b05a45030c387bc20c14e7e86
pieter <dockx@pm.me>
2023-08-30 17:07:06 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+from random import randint
+import turtle
+
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.point1.x < self.x < rectangle.point2.x \
+                and rectangle.point1.y < self.y < rectangle.point2.y:
+            return True
+        else:
+            return False
+
+
+class Rectangle:
+
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def area(self):
+        return (self.point2.x - self.point1.x) * \
+            (self.point2.y - self.point1.y)
+
+
+class GuiRectangle(Rectangle):
+
+    def draw(self, canvas):
+        # go to a certain coordinate
+        canvas.penup()  # make line disappear by holding up pen
+        canvas.goto(self.point1.x, self.point1.y)
+
+        # create rectangle
+        canvas.pendown()
+        canvas.forward(self.point2.x - self.point1.x)  # move width
+        canvas.left(90)  # turn 90 degrees left
+        canvas.forward(self.point2.y - self.point1.y)
+        canvas.left(90)
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+
+
+class GuiPoint(Point):
+
+    def draw(self, canvas, size=5, color="red"):
+        canvas.penup()
+        canvas.goto(self.x, self.y)
+        canvas.pendown()
+        canvas.dot(size, color)
+
+
+# Create rectangle object
+rectangle = GuiRectangle(Point(randint(0, 400), randint(0, 400)),
+                      Point(randint(10, 400), randint(10, 400)))
+
+# Print rectangle coordinates
+print("Rectangle Coordinates: ",
+      rectangle.point1.x, ",",
+      rectangle.point1.y, "and",
+      rectangle.point2.x, ",",
+      rectangle.point2.y)
+
+# Get point and area from user
+user_point = GuiPoint(float(input("Guess x: ")), float(input("Guess y: ")))
+user_area = float(input("Guess rectangle area: "))
+
+# Print out the game result
+print("Your point was inside rectangle: ",
+      user_point.falls_in_rectangle(rectangle))
+print("Your area was off by: ", rectangle.area() - user_area)
+
+# draw rectangle + point
+myturtle = turtle.Turtle()
+rectangle.draw(canvas=myturtle)
+user_point.draw(canvas=myturtle)
+turtle.done()
